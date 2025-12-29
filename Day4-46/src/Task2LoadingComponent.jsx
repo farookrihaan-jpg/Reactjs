5b85df0690d9cd27602709a0a554f8f8b707ca25
farookrihaan-jpg <farookrihaan@gmail.com>
2025-12-29 09:38:46 +0530

    .

--- Day4-46/src/Task2LoadingComponent.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export default function Task2LoadingComponent() {
-  useEffect(() => {
-    console.log("Component Loaded");
-  }, []);
-
-  return <div>Componentloaded</div>;
-}
